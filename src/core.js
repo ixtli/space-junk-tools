fbe354e526a84b8caecc50c29acf093f040e45a1
ixtli <christophergalardi@gmail.com>
2014-05-04 00:15:08 -0400

    initial commit

---
+++ src/core.js
@@ -0,0 +1,30 @@
+
+define(['jquery', 'rsvp', 'lodash'], function($, RSVP, _) {
+
+'use strict';
+
+
+/**
+ * Top level object constructor
+ * @constructor
+ */
+function SpaceJunk()
+{
+
+}
+
+
+/**
+ * Initialization function
+ * @return {boolean} true if nothing went wrong
+ */
+SpaceJunk.prototype.init = function()
+{
+	return true;
+};
+
+
+return SpaceJunk;
+
+
+});
---
+++ src/main.js
@@ -0,0 +1,21 @@
+
+requirejs.config({
+
+	packages: [
+		{ name: 'lodash', location: '../bower_components/lodash-amd/modern' }
+	],
+
+	paths: {
+		bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+		rsvp: '../bower_components/rsvp/rsvp.amd',
+	}
+
+});
+
+require(['core'], function(SpaceJunk)
+{
+
+	window.sj = new SpaceJunk();
+	window.sj.init();
+
+});
